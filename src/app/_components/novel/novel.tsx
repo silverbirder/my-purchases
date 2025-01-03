5007e31c49d9aad980b4d8df660511fda171a19a
shibamoto <silverbirder@gmail.com>
2025-01-03 13:38:25 +0900

    deps: npm install @tiptap/react @tiptap/pm @tiptap/starter-kit

--- src/app/_components/novel/novel.tsx
+++ src/app/_components/novel/novel.tsx
@@ -13,10 +13,21 @@ import { useState } from "react";
 import { defaultExtensions } from "./extensions";
 import { handleCommandNavigation } from "novel/extensions";
 import { slashCommand, suggestionItems } from "./slash";
+import { useDebouncedCallback } from "use-debounce";
+import { EditorEvents } from "@tiptap/react";
 
 export const Novel = () => {
   const [content, setContent] = useState<JSONContent | undefined>(undefined);
   const extensions = [...defaultExtensions, slashCommand];
+
+  const debouncedUpdates = useDebouncedCallback(
+    async ({ editor }: EditorEvents["update"]) => {
+      const json = editor.getJSON();
+      setContent(json);
+    },
+    500,
+  );
+
   return (
     <EditorRoot>
       <EditorContent
@@ -30,10 +41,7 @@ export const Novel = () => {
         }}
         extensions={extensions}
         initialContent={content}
-        onUpdate={({ editor }) => {
-          const json = editor.getJSON();
-          setContent(json);
-        }}
+        onUpdate={debouncedUpdates}
         immediatelyRender={false}
       >
         <EditorCommand className="border-muted bg-background z-50 h-auto max-h-[330px] w-72 overflow-y-auto rounded-md border px-1 py-2 shadow-md transition-all">
